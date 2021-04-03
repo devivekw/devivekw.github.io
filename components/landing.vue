1e0ab8cf4a00d80007a89a4065070166c886b1ac
devivekw <vivekw1211@gmail.com>
2021-04-04 02:15:39 +0400

    setup typed.js

--- components/landing.vue
+++ components/landing.vue
@@ -1,14 +1,20 @@
 <template>
 	<div class="grid place-items-center">
 		<img :src="$store.state.darkMode ? mewhite : meblack" class="w-96 h-96" alt="Animated Picture" />
-		<div class="flex flex-col items-center space-y-2 text-2xl">
+		<div class="flex flex-col items-center text-2xl">
 			<span class="font-mono">Hey, I'm</span>
-			<span class="text-3xl font-medium">Vivek Wadhwani</span>
+			<span class="mt-4 text-3xl font-medium">Vivek Wadhwani</span>
+			<span class="flex items-center mt-6 font-mono text-lg">
+				<span class="mr-3">></span>
+				<div ref="typeme" id="typeme" class="typeme"></div
+			></span>
 		</div>
 	</div>
 </template>
 
 <script>
+import Typed from 'typed.js';
+
 import meblack from '../assets/content/meblack.png';
 import mewhite from '../assets/content/mewhite.png';
 
@@ -18,6 +24,18 @@ export default {
 			meblack,
 			mewhite,
 		};
+	},
+
+	mounted() {
+		new Typed(this.$refs.typeme, {
+			strings: [
+				'I had a typing speed of <b class="p-1 bg-gray-100 rounded-md dark:bg-gray-800">78wpm</b>',
+				'I have a typing speed of <b class="p-1 bg-gray-100 rounded-md dark:bg-gray-800">89wpm</b>',
+				'I want a typing speed of <b class="p-1 bg-gray-100 rounded-md dark:bg-gray-800">121wpm</b>',
+			],
+			typeSpeed: 40,
+			loopCount: Infinity,
+		});
 	},
 };
 </script>
