9c31091f6c30ac9c1a82b424355f896d108fa489
devivekw <vivekw1211@gmail.com>
2021-11-24 12:03:28 +0400

    events with sa

--- plugins/simple-analytics.js
+++ plugins/simple-analytics.js
@@ -2,4 +2,7 @@
 import SimpleAnalytics from 'simple-analytics-vue';
 import Vue from 'vue';
 
-Vue.use(SimpleAnalytics, { skip: process.env.NODE_ENV !== 'production' });
+Vue.use(SimpleAnalytics, {
+	skip: process.env.NODE_ENV !== 'production',
+	domain: 'sa.devivek.live',
+});
